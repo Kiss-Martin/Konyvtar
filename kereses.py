928a2bacb7edbe02b4fb508a0b91c7ab3e424d5f
Kiss-Martin <kiss.martin@students.jedlik.eu>
2022-11-09 12:57:22 +0100

    Tesztfájl törlése

--- kereses.py
+++ kereses.py
@@ -1,27 +1,6 @@
 from classes import * 
 from functions import *
 
-def KeresesMenu():
-    print('1. Keresés cím alapján')
-    print('2. Keresés író alapján')
-    print('3. Keresés kiadás éve alapján')
-    print('4. Keresés azonosító alapján')
-    print('0. Kilépés a keresésből')
-
-    choice = input('\nVálasztás (1-4): ')
-    return int(choice)
-
-def CimAlapjan():
-    cim = input('Írja be a könyv címét: ')
-    for r in konyvek:
-        if cim.lower() in r.nev.lower():
-            print(f'{r.szerzo}: {r.nev}')
-            if r.kolcsonozve == 'igen':
-                print('A könyv nem kölcsönezhető jelenleg')
-                input('\n')
-            else:
-                print('A könyv jelenleg kölcsönözhető\n')
-                input('\n')
 
 def IroAlapjan():
     szerzo = input('Írja be a könyv íróját: ')
