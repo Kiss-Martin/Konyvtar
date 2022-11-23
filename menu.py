38366f672c18bc22f2812a11fd3cfaa3fec8dc23
Kiss-Martin <kiss.martin@students.jedlik.eu>
2022-11-23 10:27:59 +0100

    Új menüpont hozzáadása

--- menu.py
+++ menu.py
@@ -3,13 +3,14 @@ import os
 def menu():    
     os.system('cls')
     print('1. Könyvek kölcsönzése')
-    print('2. Könyvek keresése')
-    print('3. Kölcsönzők keresése\n')
+    print('2. Könyvek visszahozása')
+    print('3. Könyvek keresése')
+    print('4. Kölcsönzők keresése\n')
     print('0. Kilépés a programból')
 
-    choice = input('\nVálasztás (0-3): ')
-    while len(choice) != 1 or '0' > choice or '3' < choice:
-        choice = input('\nVálasztás (0-3): ')
+    choice = input('\nVálasztás (0-4): ')
+    while len(choice) != 1 or '0' > choice or '4' < choice:
+        choice = input('\nVálasztás (0-4): ')
     
     os.system('cls')
     return int(choice)
